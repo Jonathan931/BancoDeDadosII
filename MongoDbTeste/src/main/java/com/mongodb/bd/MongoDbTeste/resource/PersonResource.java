0a0bd2c9b8d481221a04198cabee46c487f60262
Jonathan931 <30840809+Jonathan931@users.noreply.github.com>
2018-10-23 16:18:30 -0300

    Implementando contagem
    Colocando função de contagem de tempo

--- MongoDbTeste/src/main/java/com/mongodb/bd/MongoDbTeste/resource/PersonResource.java
+++ MongoDbTeste/src/main/java/com/mongodb/bd/MongoDbTeste/resource/PersonResource.java
@@ -1,5 +1,7 @@
 package com.mongodb.bd.MongoDbTeste.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,6 @@ import com.mongodb.bd.MongoDbTeste.model.Person;
 import com.mongodb.bd.MongoDbTeste.repository.PersonRepository;
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("api/person")
@@ -29,8 +30,11 @@ public class PersonResource {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Mono<Person> criar( @RequestBody Person person){
-		return persons.save(person);
+	public Flux<Person> criar( @RequestBody List<Person> person){
+		long tempoInicio = System.currentTimeMillis();
+		Flux<Person> pessoas = persons.saveAll(person);
+		System.out.println("Tempo Total: "+(System.currentTimeMillis()-tempoInicio));
+		return pessoas;
 	}
 
 }
